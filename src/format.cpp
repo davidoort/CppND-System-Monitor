5cd09b41d1cdf520bd98d7966c6e190e4056582c
davidoort <davidoort@hotmail.com>
2020-05-15 11:23:29 +0200

    debug formatting

--- src/format.cpp
+++ src/format.cpp
@@ -1,40 +1,35 @@
-#include <string>
-#include <iostream>
-
 #include "format.h"
+
+#include <iostream>
+#include <string>
 
 using std::string;
 
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds) { 
-    int hh{0};
-    int mm{0};
-    int ss{0};
+string Format::ElapsedTime(long seconds) {
+  int hh{0};
+  int mm{0};
+  int ss{0};
 
+  hh = static_cast<int>(seconds / 3600.0);
+  int remainder_seconds = hh == 0 ? seconds : static_cast<int>(seconds) % (hh * 3600);
+  mm = static_cast<int>(remainder_seconds / (60.0));
+  ss = mm == 0 ? remainder_seconds : remainder_seconds % (mm * 60);
 
-    std::cout << seconds << std::endl;
-
-    hh = static_cast<int>(seconds/3600.0);
-    int remainder_seconds = static_cast<int>(seconds) % hh;
-    std::cout << remainder_seconds << std::endl;
-    mm = static_cast<int>(remainder_seconds/(60.0));
-    ss = remainder_seconds % mm;
-
-    string output = StringTime(hh) + ":" + StringTime(mm) + ":" + StringTime(ss);
-    return output; 
+  string output = StringTime(hh) + ":" + StringTime(mm) + ":" + StringTime(ss);
+  return output;
 }
 
 // INPUT: int measuring seconds, minutes or hours
 // OUTPUT: string with a 2-digit formatted time unit
 string Format::StringTime(int time_unit) {
-    if (time_unit == 0) {
-        return "00";
-    } else if (time_unit < 10) {
-        return "0" + std::to_string(time_unit);
-    } else {
-        return std::to_string(time_unit).substr(0, 2);
-    }
-
+  if (time_unit == 0) {
+    return "00";
+  } else if (time_unit < 10) {
+    return "0" + std::to_string(time_unit);
+  } else {
+    return std::to_string(time_unit);
+  }
 }
--- test/utest_system_monitor.cpp
+++ test/utest_system_monitor.cpp
@@ -16,6 +16,17 @@
 // };
 
 // Test the Format methods.
-TEST(SystemMonitorTest, TestFormatting) {
-    EXPECT_EQ(Format::ElapsedTime(120), "00:01:00");
+TEST(FormattingTest, TestStringTime) {
+    EXPECT_EQ(Format::StringTime(0), "00");
+    EXPECT_EQ(Format::StringTime(9), "09");
+    EXPECT_EQ(Format::StringTime(14), "14");
+    EXPECT_EQ(Format::StringTime(120), "120");
+}
+
+TEST(FormattingTest, TestElapsedTime) {
+    EXPECT_EQ(Format::ElapsedTime(0), "00:00:00");
+    EXPECT_EQ(Format::ElapsedTime(1), "00:00:01");
+    EXPECT_EQ(Format::ElapsedTime(120), "00:02:00");
+    EXPECT_EQ(Format::ElapsedTime(3601), "01:00:01");
+    EXPECT_EQ(Format::ElapsedTime(3600*260+60*44+15), "260:44:15");
 }

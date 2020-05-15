7f0e3f13e96e2992a2948546241d4fc993ca12a6
davidoort <davidoort@hotmail.com>
2020-05-15 08:59:50 +0200

    implement total processes

--- src/system.cpp
+++ src/system.cpp
@@ -25,14 +25,14 @@ std::string System::Kernel() { return LinuxParser::Kernel(); }
 // TODO: Return the system's memory utilization
 float System::MemoryUtilization() { return 0.0; }
 
-// TODO: Return the operating system name
+// Return the operating system name
 std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
 // TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { return 0; }
 
-// TODO: Return the total number of processes on the system
-int System::TotalProcesses() { return 0; }
+// Return the total number of processes on the system
+int System::TotalProcesses() { return LinuxParser::TotalProcesses(); }
 
 // TODO: Return the number of seconds since the system started running
 long int System::UpTime() { return 0; }

377d0423eb2f63d04fe1d1afe9a0fb333897d98b
davidoort <davidoort@hotmail.com>
2020-05-15 18:30:10 +0200

    implement process command

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -190,9 +190,15 @@ int LinuxParser::RunningProcesses() {
   return 0;
 }
 
-// TODO: Read and return the command associated with a process
-// REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::Command(int pid [[maybe_unused]]) { return string(); }
+// Read and return the command associated with a process
+string LinuxParser::Command(int pid) { 
+  string command;
+  std::ifstream stream(kProcDirectory + to_string(pid) + kCmdlineFilename);
+  if (stream.is_open()) {
+    std::getline(stream, command);
+    return command;
+  }
+}
 
 // TODO: Read and return the memory used by a process
 // REMOVE: [[maybe_unused]] once you define the function
